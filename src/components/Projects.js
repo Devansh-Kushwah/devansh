15cd6c52b8e15084dab553eb3a334ed814e6ee2b
Devansh-Kushwah <devanshkushwah222@gmail.com>
2023-05-18 13:57:27 +0530

    okayy

---
+++ src/components/Projects.js
@@ -0,0 +1,98 @@
+import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
+import { ProjectCard } from "./ProjectCard";
+import projImg1 from "../assets/img/project-img1.jpeg";
+import projImg2 from "../assets/img/project-img2.jpg";
+import projImg3 from "../assets/img/project-img3.jpg";
+import projImg4 from "../assets/img/project-img4.png";
+import projImg5 from "../assets/img/project-img5.png";
+import colorSharp2 from "../assets/img/color-sharp2.png";
+import 'animate.css';
+import TrackVisibility from 'react-on-screen';
+
+export const Projects = () => {
+
+  const projects = [
+    {
+      title: "FIFA Dataset Analysis",
+      description: "Data virtualization & Python",
+      imgUrl: projImg1,
+      projectLink: "#project"
+    },
+    {
+      title: "Olympics Dataset Analysis",
+      description: "Data virtualization & Python",
+      imgUrl: projImg2,
+      projectLink: "#project"
+    },
+    {
+      title: "IMDB Dataset Analysis",
+      description: "Python & SQL",
+      imgUrl: projImg3,
+      projectLink: "#project"
+    },
+    {
+      title: "Quizzical",
+      description: "React-js",
+      imgUrl: projImg4,
+      projectLink: "https://vercel.com/devansh-kushwah/quizzical"
+    },
+    {
+      title: "CountDown Timer",
+      description: "HTML, CSS, JS",
+      imgUrl: projImg5,
+      projectLink: "https://launch-countdown-timer-main-six-rho.vercel.app/"
+    },
+  ];
+
+  return (
+    <section className="project" id="project">
+      <Container>
+        <Row>
+          <Col size={12}>
+            <TrackVisibility>
+              {({ isVisible }) =>
+              <div className={isVisible ? "animate__animated animate__fadeIn": ""}>
+                <h2>Projects</h2>
+                <p> Although I have not yet completed any internships, I have gained valuable experience through various projects that have honed my technical and teamwork skills.</p>
+                <Tab.Container id="projects-tabs" defaultActiveKey="first">
+                  <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
+                    <Nav.Item>
+                      <Nav.Link eventKey="first">Tab 1</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                      <Nav.Link eventKey="second">Tab 2</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                      <Nav.Link eventKey="third">Tab 3</Nav.Link>
+                    </Nav.Item>
+                  </Nav>
+                  <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
+                    <Tab.Pane eventKey="first">
+                      <Row>
+                        {
+                          projects.map((project, index) => {
+                            return (
+                              <ProjectCard
+                                key={index}
+                                {...project}
+                                />
+                            )
+                          })
+                        }
+                      </Row>
+                    </Tab.Pane>
+                    <Tab.Pane eventKey="section">
+                    </Tab.Pane>
+                    <Tab.Pane eventKey="third">
+                    </Tab.Pane>
+                  </Tab.Content>
+                </Tab.Container>
+              </div>}
+            </TrackVisibility>
+          </Col>
+        </Row>
+      </Container>
+      <img className="background-image-right" src={colorSharp2}></img>
+    </section>
+  )
+}
